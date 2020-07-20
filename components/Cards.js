c30dcbbc8d7c4de28f46f5384c9647022ad42521
thayne-allen <thayne.allen@gmail.com>
2020-07-19 21:05:57 -0600

    Cards

--- components/Cards.js
+++ components/Cards.js
@@ -20,3 +20,78 @@
 // Add a listener for click events so that when a user clicks on a card, the headline of the article is logged to the console.
 //
 // Use your function to create a card for each of the articles, and append each card to the DOM.
+
+const { default: Axios } = require("axios")
+
+const create = c => document.createElement(c);
+const query = q => document.querySelector(q);
+
+const errors = query('.errors-container')
+// console.log(errors)
+const cards = query('.cards-container')
+const articleURL = 'https://lambda-times-backend.herokuapp.com/articles'
+
+Axios.get(articleURL)
+.then((res) => {
+    const articles = res.data.articles
+    for(let obj in articles){
+        articles[obj].forEach(element => {
+            cards.appendChild(cardMaker(element))
+        });
+    }
+})
+.catch((err) => {
+console.log(`error`,err)
+// console.log(err.response)
+// console.log(err.response.data)
+// console.log(err.response.status)
+// console.log(err.response.headers)
+// console.log(err.response.request.statusText)
+
+errors.appendChild(errorMessages(err))
+})
+
+
+function cardMaker(obj){
+    const card = create('div')
+    const headline = create('div')
+    const author = create('div')
+    const imgContainer = create('div')
+    const img = create('img')
+    const authorName = create('span')
+
+    headline.textContent = obj.headline
+    img.src = obj.authorPhoto
+    authorName.textContent = obj.authorName
+
+    card.className = 'card'
+    headline.className = 'headline'
+    author.className = 'author'
+    imgContainer.className = 'img-container'
+
+    card.appendChild(headline)
+    card.appendChild(author)
+    author.appendChild(imgContainer)
+    imgContainer.appendChild(img)
+    author.appendChild(authorName)
+
+    card.addEventListener('click', () => {
+        console.log(obj.headline)
+    })
+
+    return card
+}
+
+function errorMessages(err){
+    const error = create('div')
+    const status = create('h3')
+    const headers = create('p')
+
+    status.textContent = `Error ${err.response.status} ${err.response.request.statusText}`
+    headers.textContent = err.message
+
+    error.appendChild(status)
+    error.appendChild(headers)
+
+    return error
+}
--- components/Carousel.js
+++ components/Carousel.js
@@ -22,3 +22,4 @@
     <div class="right-button"> > </div>
   </div>
 */
+
